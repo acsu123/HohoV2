6038d1546e3157af3a3dde8bad891fa46a6eeb1f
Catsuq <100895355+Catsuq@users.noreply.github.com>
2022-11-27 01:54:35 -0600

    Update ScriptLoad.lua

--- ScriptLoad.lua
+++ ScriptLoad.lua
@@ -516,6 +516,10 @@ local list = {
 	
 	--GPO
 	[648454481]="loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/Free/GPOFree.lua', true))()",
+
+        --Murder Party (New)
+        [5611648039]="loadstring(game:HttpGet('https://raw.githubusercontent.com/artas01/artas01/c50dad68804d1656a15f5357f9333daa4a4f15af/MurderParty_bylolProplayerlol'))()",
+
 }
 
 local listPre = {
