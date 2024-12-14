d01ae3021926876c630179a9dd92ca5b1f890423
acsu123 <85284831+acsu123@users.noreply.github.com>
2024-12-14 12:19:14 +0700

    Update ScriptLoad.lua

--- ScriptLoad.lua
+++ ScriptLoad.lua
@@ -26,7 +26,7 @@ _G.HOHO_AD = {
 }
 local list = {
 	--GO FISHING
-	[116605585218149] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/refs/heads/Free/GOFISHING.lua'))()",
+	[6764180126] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/refs/heads/Free/GOFISHING.lua'))()",
 	
 	--Fisch
 	[5750914919] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/refs/heads/Free/Fisch.lua'))()",
