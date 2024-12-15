f66078ba402c52cfcf04bf6224fd38e06fcb3a48
acsu123 <85284831+acsu123@users.noreply.github.com>
2024-12-16 01:43:42 +0700

    Update ScriptLoad.lua

--- ScriptLoad.lua
+++ ScriptLoad.lua
@@ -6,8 +6,7 @@
 --         getgenv()[Index] = Value
 --     end
 -- end
--- Cubix dumb i have to fix it for u B(
--- getgenv().bit = bit32
+getgenv().bit = bit32 -- Cubix & Awp dumb i have to fix it for u B(
 -- cloneref
 -- local a=Instance.new("Part")for b,c in pairs(getreg())do if type(c)=="table"and#c then if rawget(c,"__mode")=="kvs"then for d,e in pairs(c)do if e==a then getgenv().InstanceList=c;break end end end end end;local f={}function f.invalidate(g)if not InstanceList then return end;for b,c in pairs(InstanceList)do if c==g then InstanceList[b]=nil;return g end end end;if not cloneref then getgenv().cloneref=f.invalidate end
 -- require
