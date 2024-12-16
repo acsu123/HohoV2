fb91eb5212212140806df8caef7acc45aab75611
acsu123 <85284831+acsu123@users.noreply.github.com>
2024-12-16 11:07:26 +0700

    Update ScriptLoad.lua

--- ScriptLoad.lua
+++ ScriptLoad.lua
@@ -354,5 +354,5 @@ local path = "wait(5);loadstring(game:HttpGet('https://raw.githubusercontent.com
 -- if getgenv().Key then
 -- 	path = "getgenv().Key = '"..getgenv().Key.."'\n"..path
 -- end
-local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
-queueteleport(path)
+-- local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
+-- queueteleport(path)
