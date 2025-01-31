dab31fada3fef9bf924fddc3764ea45bbea414e8
acsu123 <85284831+acsu123@users.noreply.github.com>
2025-01-31 17:25:35 +0700

    Update ScriptLoad.lua

--- ScriptLoad.lua
+++ ScriptLoad.lua
@@ -1,35 +1,35 @@
 -- if _G.HOHO_RAN then return end
 -- _G.HOHO_RAN = true
 -- most executors issue
-for Index, Value in next, debug do
-    if not getgenv()[Index] then
-        getgenv()[Index] = Value
-    end
-end
+-- for Index, Value in next, debug do
+--     if not getgenv()[Index] then
+--         getgenv()[Index] = Value
+--     end
+-- end
 
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 local ExecutorName = identifyexecutor()
 
-getgenv().bit = bit32 -- Cubix & Awp issue :)
+-- getgenv().bit = bit32 -- Cubix & Awp issue :)
 
-if ExecutorName:find("Swift") then -- don't ask me idk why it work :D?
-	task.spawn(function()
-	    loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/HohoHub.lua'))()
-	end)
-	wait(2.5)
-	for i, v in pairs(game.CoreGui:GetChildren()) do
-	    if v.Name:find("Hoho") then
-	        v:Destroy()
-	    end
-	end
-elseif ExecutorName:find("Seli") then -- Seliware
-	local rgenv = getrenv()
-	local renv = getrawmetatable(rgenv).__index
-	for i,v in next, renv do
-		rgenv[i] = v
-	end
-end
+-- if ExecutorName:find("Swift") then -- don't ask me idk why it work :D?
+-- 	task.spawn(function()
+-- 	    loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/HohoHub.lua'))()
+-- 	end)
+-- 	wait(2.5)
+-- 	for i, v in pairs(game.CoreGui:GetChildren()) do
+-- 	    if v.Name:find("Hoho") then
+-- 	        v:Destroy()
+-- 	    end
+-- 	end
+-- elseif ExecutorName:find("Seli") then -- Seliware
+-- 	local rgenv = getrenv()
+-- 	local renv = getrawmetatable(rgenv).__index
+-- 	for i,v in next, renv do
+-- 		rgenv[i] = v
+-- 	end
+-- end
 
 -- cloneref
 -- local a=Instance.new("Part")for b,c in pairs(getreg())do if type(c)=="table"and#c then if rawget(c,"__mode")=="kvs"then for d,e in pairs(c)do if e==a then getgenv().InstanceList=c;break end end end end end;local f={}function f.invalidate(g)if not InstanceList then return end;for b,c in pairs(InstanceList)do if c==g then InstanceList[b]=nil;return g end end end;if not cloneref then getgenv().cloneref=f.invalidate end
@@ -38,9 +38,9 @@ end
 
 local notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Notification.lua"))()
 
-if _G.HohoVersion == "v3" or _G["Fumo Blox Fruit - HoHo Hub Kaitun V3"] or _G["HoHo Hub Auto Bounty V3"] or _G["HoHo Fruits Collector"] or _G.HOHO_PVP_UI or _G.HOHO_SERVER_ID then
+-- if _G.HohoVersion == "v3" or _G["Fumo Blox Fruit - HoHo Hub Kaitun V3"] or _G["HoHo Hub Auto Bounty V3"] or _G["HoHo Fruits Collector"] or _G.HOHO_PVP_UI or _G.HOHO_SERVER_ID then
 	-- return loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HohoV2/Free/BloxFruitFreeV3.lua"))()
-end
+-- end
 
 -- _G.HOHO_AD = {
 -- 	[1] = {link="https://gcdnb.pbrd.co/images/AVitm37MSdIY.png", exp=tick()+9999, read = "https://discord.gg/CWvQUcfr25"},
