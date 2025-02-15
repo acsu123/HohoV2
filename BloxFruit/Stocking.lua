9c91ab99bef669cee41e81cfe53b9e7d2368f56d
acsu123 <85284831+acsu123@users.noreply.github.com>
2025-02-15 14:12:11 +0700

    Update Stocking.lua

--- BloxFruit/Stocking.lua
+++ BloxFruit/Stocking.lua
@@ -1,1 +1,3 @@
--- loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/74e0db881cff709417b4c375d8dffa79.lua"))()
+task.delay(3, function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/74e0db881cff709417b4c375d8dffa79.lua"))()
+end)
