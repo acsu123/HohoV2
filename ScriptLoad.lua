2e1a04e3f8ba7db0f690d07d71127a658d869148
acsu123 <85284831+acsu123@users.noreply.github.com>
2025-07-01 13:15:21 +0700

    Update ScriptLoad.lua

--- ScriptLoad.lua
+++ ScriptLoad.lua
@@ -23,6 +23,7 @@ local GameList = {
 	[3359505957] = "095fbd843016a7af1d3a9ee88714c64a", -- Collect All Pets
 	[6167925365] = "e220573a9f986e150c6af8d4d1fb9b7c", -- Cong Dong Viet Nam
 	[5361032378] = "ff4e04500b94246eaa3f5c5be92a8b4a", -- Sol's RNG
+	[7709344486] = "1d5eea7e66ccb5ca4d11c26ff2d4c6b1", -- Steal a Brainrot
 }
 
 for id, scriptid in pairs(GameList) do
