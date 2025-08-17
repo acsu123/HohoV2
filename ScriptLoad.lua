f9f26605e2999a542deef83a10f9222e184c3550
acsu123 <85284831+acsu123@users.noreply.github.com>
2025-08-17 07:08:07 +0700

    Update ScriptLoad.lua

--- ScriptLoad.lua
+++ ScriptLoad.lua
@@ -27,6 +27,7 @@ local GameList = {
 	[7326934954] = "0aa67223637322085cfeaf80ae9af69f", -- 99 Nights in the Forest
 	[3149100453] = "dbe59157859f6030587fd61ad4faad75", -- Eat Blob Simulator
 	[5995470825] = "83363ffca1175ef0c06d4028b77061a4", -- Hypershot
+	[358276974] = "23e50d188c7e27477a1c6eacb076e2ba", -- Apocalypse Rising 2
 }
 
 for id, scriptid in pairs(GameList) do
