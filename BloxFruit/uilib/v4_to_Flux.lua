897f40df7f671b1534d7caa8dbc2cec8724dc1c3
acsu123 <85284831+acsu123@users.noreply.github.com>
2025-11-22 21:19:40 +0700

    Update v4_to_Flux.lua

--- BloxFruit/uilib/v4_to_Flux.lua
+++ BloxFruit/uilib/v4_to_Flux.lua
@@ -2877,6 +2877,8 @@ function Window()
                     tab:Button(title, title, function()
                         callback()
                     end)
+                    local Config = {Object=obj, ToFrame=obj}
+                    return Config
                 end
 
                 function MiniCreateButtonToFrameReturn:SelectorMultiple(title,whichTab,__List,callback, listhave)
