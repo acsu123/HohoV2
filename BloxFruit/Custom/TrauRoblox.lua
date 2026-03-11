8776bbef992e097c0867dd3082c26220882f66f4
acsu123 <85284831+acsu123@users.noreply.github.com>
2026-03-11 17:52:20 +0700

    Create TrauRoblox.lua

---
+++ BloxFruit/Custom/TrauRoblox.lua
@@ -0,0 +1,24 @@
+IS_CUSTOM_UI_MODE = true
+CUSTOM_UI_MODE_DATA = {
+    Name = "TRAU",
+    Theme = "Cyan",
+    Logo = "rbxassetid://115681779020519",
+    Background = "rbxassetid://71737189750485",
+    YouTube = "Trauroblox2004",
+    Discord = "6f4bDdwDfa",
+    WelcomeMessage = {
+        "TRAU HUB",
+        "BY TRAU RBX"
+    },
+    TrailIcon = "🧊",
+    ToggleIcon = "rbxassetid://137421482806874",
+    CommunityIcon = "rbxassetid://71737189750485",
+    Contributers = {
+        "Trau Roblox"
+    },
+    IntroText = "Preparing your HUB for an amazing experience.",
+    GetKeyLink = "https://hehehub-acsu123.pythonanywhere.com/api/getkeyv4?hwid=BG7w89RuTxxtugtJH04EijntTZ2m72Sf-traurobloxStep1",
+    TutorialLink = "https://hehehub-acsu123.pythonanywhere.com/api/getkey?hwid=TUTORIAL",
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
