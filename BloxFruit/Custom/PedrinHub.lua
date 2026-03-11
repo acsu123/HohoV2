654d60751db2c2e5b753e22a9be97e779a67cf8a
acsu123 <85284831+acsu123@users.noreply.github.com>
2026-03-11 22:37:35 +0700

    Create PedrinHub.lua

---
+++ BloxFruit/Custom/PedrinHub.lua
@@ -0,0 +1,24 @@
+IS_CUSTOM_UI_MODE = true
+CUSTOM_UI_MODE_DATA = {
+    Name = "PEDRIN",
+    Theme = "Purple",
+    Logo = "rbxassetid://124691103515236",
+    Background = "rbxassetid://74795247699120",
+    YouTube = "PedrinTravasOFC",
+    Discord = "rCZ7uBsNFS",
+    WelcomeMessage = {
+        "PEDRIN HUB",
+        "BY PEDRIN TRAVAS"
+    },
+    TrailIcon = "😈",
+    ToggleIcon = "rbxassetid://102641055452421",
+    CommunityIcon = "rbxassetid://74795247699120",
+    Contributers = {
+        "PEDRIN TRAVAS OFC | hashishx"
+    },
+    IntroText = "Preparing your HUB for an amazing experience.",
+    GetKeyLink = "https://hehehub-acsu123.pythonanywhere.com/api/getkey?hwid=rubuhub",
+    TutorialLink = "https://hehehub-acsu123.pythonanywhere.com/api/getkey?hwid=TUTORIAL",
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1c3b3c8a311a71c8e81c44b998a81e39.lua"))()
