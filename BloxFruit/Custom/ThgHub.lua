1a5b015255a1af64ce6c5c60a7a8f9c77b59665b
acsu123 <85284831+acsu123@users.noreply.github.com>
2026-03-12 02:16:09 +0700

    Create ThgHub.lua

---
+++ BloxFruit/Custom/ThgHub.lua
@@ -0,0 +1,24 @@
+IS_CUSTOM_UI_MODE = true
+CUSTOM_UI_MODE_DATA = {
+    Name = "THG",
+    Theme = "Orange",
+    Logo = "rbxassetid://108865791285850",
+    Background = "rbxassetid://122624454026347",
+    YouTube = "Dat-THG",
+    Discord = "tts9PrnRks",
+    WelcomeMessage = {
+        "THG HUB",
+        "BY DatTHG"
+    },
+    TrailIcon = "🔥",
+    ToggleIcon = "rbxassetid://86047347388765",
+    CommunityIcon = "rbxassetid://86047347388765",
+    Contributers = {
+        "Đạt THG"
+    },
+    IntroText = "Preparing your HUB for an amazing experience.",
+    GetKeyLink = "https://hehehub-acsu123.pythonanywhere.com/api/getkeyv4?hwid=BG7w89RuTxxtugtJH04EijntTZ2m72Sf-datThgStep1",
+    TutorialLink = "https://hehehub-acsu123.pythonanywhere.com/api/getkey?hwid=TUTORIAL",
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
