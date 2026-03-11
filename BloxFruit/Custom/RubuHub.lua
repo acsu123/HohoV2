d02716c93ddaf4cf95f86977ea5f4862018d05de
acsu123 <85284831+acsu123@users.noreply.github.com>
2026-03-11 17:00:08 +0700

    Create RubuHub.lua

---
+++ BloxFruit/Custom/RubuHub.lua
@@ -0,0 +1,24 @@
+IS_CUSTOM_UI_MODE = true
+CUSTOM_UI_MODE_DATA = {
+    Name = "RUBU",
+    Theme = "Cyan",
+    Logo = "rbxassetid://105166236288009",
+    Background = "rbxassetid://108854524739326",
+    YouTube = "rubutv_roblox",
+    Discord = "hоhоhub",
+    WelcomeMessage = {
+        "RUBU HUB",
+        "BY RUBU TV"
+    },
+    TrailIcon = "😸",
+    ToggleIcon = "rbxassetid://72500346986640",
+    CommunityIcon = "rbxassetid://72500346986640",
+    Contributers = {
+        "RuBuTV Roblox"
+    },
+    IntroText = "Preparing your HUB for an amazing experience.",
+    GetKeyLink = "https://hehehub-acsu123.pythonanywhere.com/api/getkeyv4?hwid=BG7w89RuTxxtugtJH04EijntTZ2m72Sf-rubuhubStep1",
+    TutorialLink = "https://hehehub-acsu123.pythonanywhere.com/api/getkey?hwid=TUTORIAL",
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
