cd584a368a449949e96b6f66e43497c68bfc1fb0
acsu123 <85284831+acsu123@users.noreply.github.com>
2026-08-24 00:29:55 +0700

    Delete BloxFruit/preload/vim.lua

--- BloxFruit/preload/vim.lua
+++
@@ -1,6 +0,0 @@
-local VirtualInputManager = Instance.new("VirtualInputManager")
-_G.VIM = VirtualInputManager
-_G.VIM_CLICK = function()
-    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.ButtonR2, false, game)
-    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.ButtonR2, false, game)
-end
